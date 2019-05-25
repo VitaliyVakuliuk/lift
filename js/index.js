9aa7572ad01b5d37a36dde130ab2ecae290a2cb4
VitaliyVakuliuk <vitaliyvak@gmail.com>
2019-05-25 18:05:01 +0300

    chabge index.js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
-import  { NUMBER_FLOORS } from "../constants.js";
+//import  { NUMBER_FLOORS } from "../constants.js";
 
 function createElements(){
     const parentElem = document.querySelector(".block");
-    for(let i = 1; i <= NUMBER_FLOORS; i++){
+    for(let i = 1; i <= 5; i++){
         let elem = document.createElement("div");
         elem.className = "floor";
-        elem.innerHTML = "floor" + " " +i;
+        elem.innerHTML = `floor ${i}`;
         parentElem.appendChild(elem);
     }
 }
@@ -13,8 +13,17 @@ createElements();
 
 function callLift(floor){
     setTimeout(()=>{
-        alert("Ліфт прибув Двері відкрито");
+        alert(`Ліфт прибув Двері відкрито ${floor}`);
     },2000); // todo time change after create all floors
 };
 
 
+//callLift("f");
+
+let asd = document.querySelectorAll('.floor')
+    .forEach( btn => btn.addEventListener('click', func));
+
+function func(e){
+    callLift(e.target.innerHTML);
+};
+console.log(asd)
