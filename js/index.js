e34fea3d962e0aa6b3b3e236d39f6ec69916d6b8
VitaliyVakuliuk <vitaliyvak@gmail.com>
2019-05-25 14:30:22 +0300

    creat function

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-alert("something");
+function func(){
+    for(let i = 1; i < 10; i++){
+        let elem = document.createElement('div');
+        let num = document.createTextNode(i);
+        elem.appendChild(num);
+        document.body.appendChild(elem);
+    }
+};
+func();
