d5ce45e1de74b983d6e28f0dfa5380ac2839ae6a
Igor Mocherad <mocherad86@gmail.com>
2019-05-25 14:35:33 +0300

    create function createTable

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-alert("something");
+function createTable(){
+    let parentElem = document.querySelector(".block");
+    for(let i = 0; i < 10; i++){
+        let elem = document.createElement("div");
+        elem.innerHTML = i;
+        parentElem.appendChild(elem);
+    }
+}
+createTable();
