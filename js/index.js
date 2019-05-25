9479d4136a25ce1ed8a864c6a3b489e5206e90c8
VitaliyVakuliuk <vitaliyvak@gmail.com>
2019-05-25 15:03:38 +0300

    create new function

--- js/index.js
+++ js/index.js
@@ -1,11 +1,15 @@
 
 function createTable(){
     const parentElem = document.querySelector(".block");
-    for(let i = 0; i < 10; i++){
+    for(let i = 1; i < 6; i++){
         let elem = document.createElement("div");
+        let floor = document.createElement("class");
+        floor.className = "floor";
+        elem.appendChild(floor);
         elem.innerHTML = i;
         parentElem.appendChild(elem);
     }
 }
 createTable();
+
 
