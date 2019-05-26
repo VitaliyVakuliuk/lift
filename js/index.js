575d5abb795fe174094886d9198c917955a244a0
VitaliyVakuliuk <vitaliyvak@gmail.com>
2019-05-26 23:32:43 +0300

    change index.js

--- js/index.js
+++ js/index.js
@@ -1,21 +1,21 @@
 //import  { NUMBER_FLOORS } from "../constants.js";
 
-function createElements(display = null, parentEl = "#block"){
+/*function createElements(display = null, parentEl = "#block"){
     const parentElem = document.querySelector(parentEl);
 
     for(let i = 1; i <= 5; i++){
         let elem = document.createElement("div");
-        elem.className = display || "floor";
-        elem.innerHTML = display ? i : `floor ${i}`;
+        elem.className = display;
+        elem.innerHTML = i;
         parentElem.appendChild(elem);
     }
-}
-createElements();
+}*/
+//createElements();
 
 function callLift(floor){
     setTimeout(()=>{
-        alert(`Ліфт прибув Двері відкрито ${floor}`);
-        showDisplay();
+        alert('Ліфт прибув Двері відкрито');
+        goLift();
 
     },2000); // todo time change after create all floors
 };
@@ -23,19 +23,33 @@ function callLift(floor){
 
 //callLift("f");
 
-let asd = document.querySelectorAll('.floor')
+/*let asd = document.querySelectorAll('.floor')
     .forEach( btn => btn.addEventListener('click', func));
 
 function func(e){
     callLift(e.target.innerHTML);
-};
+};*/
 
-function showDisplay() {
+let buttons = document.querySelectorAll('.btn')
+    .forEach(button => button.addEventListener('click', function(){
+        alert(`Ви вибрали ${button.innerHTML} поверх`);
+        callLift();
+}));
+
+document.querySelector('#block')
+    .addEventListener('click', function(){
+        callLift();
+    })
+
+
+/*function showDisplay() {
     goLift();
     createElements("btn","#display");
-};
+};*/
 
 function goLift(){
-    let element1 = document.querySelector('#block');
+    let element = document.querySelector('#block');
+    let element1 = document.querySelector('#display')
+    element.classList.toggle("hide");
     element1.classList.toggle("hide");
-}
+};
