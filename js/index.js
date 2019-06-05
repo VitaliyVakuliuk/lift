ae0ba58437e8ad601d385032331e6748723ea928
VitaliyVakuliuk <vitaliyvak@gmail.com>
2019-06-06 00:15:02 +0300

    change index.js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
 let door = document.getElementById("door");
-function Resize() {
-    let ob = setInterval(function () {
-        door.style.width = parseInt(door.style.width) - 50 + "px";
-        count++;
-    }, 100);
+let door1 = document.getElementById("door_left");
+function resize() {
+    setTimeout(()=>{
+        for(let i = 250, k = 250 ; i > door.style.width.length; i--, k--){
+        door.style.width = parseInt(door.style.width) - i + "px";
+        door1.style.width = parseInt(door1.style.width) - i + "px";
+        };
+        goLift();
+        scoreboard.value = `Ви у ліфті, виберіть поверх`;
+    }, 3000);
 };
 
 let scoreboard = document.querySelector('#scoreboard');
@@ -14,25 +19,27 @@ scoreboard.value = `ЛІФТ НА ${getRandomInt(1, 5)} ПОВЕРСІ`
 
 function callLift(floor){
     setTimeout(()=>{
-        alert('Ліфт прибув Двері відкрито');
-        goLift();
-
+        scoreboard.value = `Ліфт прибув Двері відкриваються`;
     },2000); // todo time change after create all floors
 };
 
 let buttons = document.querySelectorAll('.btn')
-    .forEach(button => button.addEventListener('click', function(){
-        alert(`Ви вибрали ${button.innerHTML} поверх`);
-        setTimeout(()=>{
-            alert(`Ліфт прибув на ${button.innerHTML} поверх`);
-            goLift();
+    .forEach(button => button.addEventListener('click', function () {
+        scoreboard.value = `Ви вибрали ${button.innerHTML} поверх`;
+        setTimeout(() => {
+            scoreboard.value = `Ліфт прибув на ${button.innerHTML} поверх`;
         }, 2000);
-}));
+        setTimeout(() => {
+            goLift();
+            scoreboard.value = `ЛІФТ НА ${button.innerHTML} ПОВЕРСІ`
+        }, 4000);
+
+    }));
 
 document.querySelector('#block')
     .addEventListener('click', function(){
         callLift();
-        Resize();
+        resize();
     })
 
 function goLift(){
@@ -41,4 +48,5 @@ function goLift(){
     element.classList.toggle("hide");
     element1.classList.toggle("hide");
     door.classList.toggle('hide')
+    door1.classList.toggle('hide');
 };
