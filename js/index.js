4f405852aac876855439dc5aa05f4b9b7ad339c0
VitaliyVakuliuk <vitaliyvak@gmail.com>
2019-06-12 11:35:28 +0300

    change

--- js/index.js
+++ js/index.js
@@ -1,53 +1,64 @@
-let door = document.getElementById("door_right");
-let door1 = document.getElementById("door_left");
-function resize() {
-    setTimeout(()=>{
-        for(let i = 250, k = 250 ; i > door.style.width.length; i--, k--){
-        door.style.width = parseInt(door.style.width) - i + "px";
-        door1.style.width = parseInt(door1.style.width) - i + "px";
-        };
+function openDoorLift() {
+    setTimeout(() => {
+        $('#door_left').animate({ left: '-250px' });
+        $('#door_right').animate({ right: '-250px' });
+    }, 3000);
+    setTimeout(() => {
         goLift();
         scoreboard.value = `Ви у ліфті, виберіть поверх`;
-    }, 3000);
+    }, 4000);
+};
+
+function shutDoorLift() {
+    setTimeout(() => {
+        $('#door_left').animate({ left: '0px' });
+        $('#door_right').animate({ right: '0px' });
+        scoreboard.value = `обережно двері закриваються`;
+    }, 2000);
 };
 
 let scoreboard = document.querySelector('#scoreboard');
-function getRandomInt(min, max) { 
-    return Math.floor(Math.random() * Math.floor(max - min + 1)) + min; 
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * Math.floor(max - min + 1)) + min;
 };
 scoreboard.value = `ЛІФТ НА ${getRandomInt(1, 5)} ПОВЕРСІ`
 
-function callLift(floor){
-    setTimeout(()=>{
+function callLift(floor) {
+    setTimeout(() => {
         scoreboard.value = `Ліфт прибув Двері відкриваються`;
-    },2000); // todo time change after create all floors
+    }, 2000); // to do time change after create all floors
 };
 
 let buttons = document.querySelectorAll('.btn')
     .forEach(button => button.addEventListener('click', function () {
         scoreboard.value = `Ви вибрали ${button.innerHTML} поверх`;
+        shutDoorLift();
+        setTimeout(() => {
+            scoreboard.value = `Ліфт їде на ${button.innerHTML} поверх`;
+        }, 4000);
         setTimeout(() => {
             scoreboard.value = `Ліфт прибув на ${button.innerHTML} поверх`;
-        }, 2000);
+            $('#door_left').animate({ left: '-250px' });
+            $('#door_right').animate({ right: '-250px' });
+        }, 6000);
         setTimeout(() => {
             goLift();
+            $('#door_left').animate({ left: '0px' });
+            $('#door_right').animate({ right: '0px' });
             scoreboard.value = `ЛІФТ НА ${button.innerHTML} ПОВЕРСІ`
-        }, 4000);
-
+        }, 8000);
     }));
 
 document.querySelector('#block')
-    .addEventListener('click', function(){
+    .addEventListener('click', function () {
         scoreboard.value = `Ліфт опускається`;
         callLift();
-        resize();
+        openDoorLift()
     })
 
-function goLift(){
+function goLift() {
     let element = document.querySelector('#block');
     let element1 = document.querySelector('#display')
     element.classList.toggle("hide");
     element1.classList.toggle("hide");
-    door.classList.toggle('hide')
-    door1.classList.toggle('hide');
 };
